4e649fd3f26aed60ac1672a0dbed3b3573104397
thosapol <isocare.thospol@gmail.com>
2018-12-14 11:39:18 +0700

    Add func for testbot round7

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -51,15 +50,4 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJson(w, code, map[string]string{"error": msg})
-}
-
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
 }
