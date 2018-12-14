4e00a693f0e3fe723123175bbfad3720d5c59902
thosapol <isocare.thospol@gmail.com>
2018-12-14 10:54:53 +0700

    Add func for testbot

--- main.go
+++ main.go
@@ -32,4 +32,23 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello")
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
 }
